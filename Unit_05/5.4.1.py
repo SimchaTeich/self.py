480a1ecea643ae1a1217fd11455352d28ecf2ac1
SimchaTeich <sim.teich@gmail.com>
2023-03-19 02:40:59 +0200

    append solution for task 5.4.1

---
+++ Unit_05/5.4.1.py
@@ -0,0 +1,18 @@
+def func(num1, num2):
+    """
+    calcs the sum of numbers.
+    :param num1: number
+    :type num1: integer
+    :param num2: number
+    :type num2: integer
+    :return: the sum of the numbers.
+    :rtype: integer
+    """
+    return num1 + num2
+
+def main():
+    print("7 + 3 = " + func(7, 3))
+
+if __name__ == "__main__":
+    main()
+   
