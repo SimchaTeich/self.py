7e199608b4de9f77d55dafd59634eb6a14bc5559
SimchaTeich <sim.teich@gmail.com>
2023-03-19 02:04:12 +0200

    append solution for task 1.4.2

---
+++ Unit_01/1.4.2.py
@@ -0,0 +1,52 @@
+ASCII_ART = r"""picture 1:
+    x-------x
+
+picture 2:
+    x-------x
+    |
+    |
+    |
+    |
+    |
+
+picture 3:
+    x-------x
+    |       |
+    |       0
+    |
+    |
+    |
+
+picture 4:
+    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |
+
+picture 5:
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |
+    |
+
+picture 6:
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      /
+    |
+
+picture 7:
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      / \
+    |"""
+
+print(ASCII_ART)
