5c6789f0931fcf45826983823c24f4f57876e01c
SimchaTeich <sim.teich@gmail.com>
2023-03-25 21:30:20 +0300

    implement task 6.4.2

---
+++ Unit_06/6.4.2.py
@@ -0,0 +1,16 @@
+def is_valid_input(letter_guessed):
+    return len(letter_guessed) == 1 and letter_guessed.isalpha()
+
+def check_valid_input(letter_guessed, old_letters_guessed):
+    letter_guessed = letter_guessed.lower()
+    return is_valid_input(letter_guessed) and letter_guessed not in old_letters_guessed
+
+
+def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+    if check_valid_input(letter_guessed, old_letters_guessed):
+        old_letters_guessed.append(letter_guessed)
+        return True
+    else:
+        print('X')
+        print(' -> '.join(sorted(old_letters_guessed)))
+        return False
