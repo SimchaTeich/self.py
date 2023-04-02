89a5024e1c5f99f411359269c9a58b1dae77b6c1
SimchaTeich <sim.teich@gmail.com>
2023-04-02 03:04:21 +0300

    implement task 9.2.2.py

--- Unit_09/9.2.2.py
+++ Unit_09/9.2.2.py
@@ -1,11 +1,4 @@
 def copy_file_content(source, destination):
-    # open files for reading and writing
-    src_file = open(source, 'r')
-    dst_file = open(destination, 'w')
-    
-    # copy content from src file to dst file
-    dst_file.write(src_file.read())
-    
-    # close the files.
-    src_file.close()
-    dst_file.close()
+    with open(source, 'r') as src_file, \
+        open(destination, 'w') as dst_file:
+        dst_file.write(src_file.read())
